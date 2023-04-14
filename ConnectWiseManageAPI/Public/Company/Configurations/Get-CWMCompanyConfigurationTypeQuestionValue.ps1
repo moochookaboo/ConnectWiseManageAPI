630ad840c420c1af6e50d6c954c750cf7b1619c9
dconasp <43192354+moochookaboo@users.noreply.github.com>
2023-04-13 23:35:38 -0500

    Update Get-CWMCompanyConfigurationTypeQuestion

--- ConnectWiseManageAPI/Public/Company/Configurations/Get-CWMCompanyConfigurationTypeQuestionValue.ps1
+++ ConnectWiseManageAPI/Public/Company/Configurations/Get-CWMCompanyConfigurationTypeQuestionValue.ps1
@@ -1,29 +1,4 @@
-﻿<# function Get-CWMCompanyConfigurationTypeQuestion {
-    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSShouldProcess', '', Justification = 'Used by sub-function')]
-    [CmdletBinding(SupportsShouldProcess, ConfirmImpact='Medium')]
-    param(
-        [Parameter(Mandatory=$true)]
-        [Alias('typeId')]
-        [int]$grandparentId,
-        [Parameter(Mandatory=$true)]
-        [Alias('questionId')]
-        [int]$parentId,
-        [int]$id,
-        [hashtable]$configurationType,
-        [boolean]$defaultFlag,
-        [boolean]$inactiveFlag,
-        [hashtable]$question,
-        [Parameter(Mandatory=$true)]
-        [ValidateLength(1,1000)]
-        [string]$value,
-        [hashtable]$_info
-    )
-
-    $Endpoint = "/company/configurations/types/$($grandparentId)/questions/$($parentId)"
-    Invoke-CWMGetMaster -Arguments $PsBoundParameters -Endpoint $Endpoint
-} #>
-
-function Get-CWMCompanyConfigurationTypeQuestionValue {
+﻿function Get-CWMCompanyConfigurationTypeQuestionValue {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true)]
